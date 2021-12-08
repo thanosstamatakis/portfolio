4aa0e2b9a98d8811121ed9299731339c59b78f4a
Thanos Stamatakis <thanos.stamatakis@gmail.com>
2021-12-08 14:27:31 +0200

    Minor layout changes.

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+    selector: 'app-experience',
+    templateUrl: './experience.component.html',
+    styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
+    public projects = {
+        gymq: {
+            numImages: 19,
+            folder: 'gymq',
+            images: [],
+        },
+        covidtracker: {
+            numImages: 7,
+            folder: 'covidtracker',
+            images: [],
+        },
+        drasys: {
+            numImages: 15,
+            folder: 'drasys',
+            images: [],
+        },
+        brightness: {
+            numImages: 4,
+            folder: 'brightness-control',
+            images: [],
+        },
+    };
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        for (const key in this.projects) {
+            if (Object.prototype.hasOwnProperty.call(this.projects, key)) {
+                let { numImages, folder } = this.projects[key];
+                let urls = [];
+                for (let i = 1; i <= numImages; i++) {
+                    urls.push({ src: `${environment.cdnBaseUrl}/${folder}/${i}` });
+                }
+                this.projects[key].images = urls;
+            }
+        }
+    }
 }
